6f42b0c1aa2b2336ab5031fc00dbcdccdb3f9262
Taiwo Ogunyemi <ogunyemitaiwojohn@gmail.com>
2025-05-21 20:58:20 +0100

    React js Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import ListGroup from "./components/ListGroup";
+import image from './assets/image.png'
+
+function App() {
+  return (
+    <>
+      {/* <img src={image} /> */}
+      <ListGroup />
+    </>
+  );
+}
+
+export default App;
